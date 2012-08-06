e0ba65ce199d439b295f87847ea20f1a02fb54ac
Nick Doiron <ndoiron@mapmeld.com>
2012-08-05 20:42:49 -0700

    update livecode viewer whenever host requests program

--- public/scripts/livestream.js
+++ public/scripts/livestream.js
@@ -1,5 +1,11 @@
 var socket = io.connect(window.location.hostname);
 socket.on('newprogram', function(data){
   console.log(data);
-  document.getElementById("livecode").innerHTML = data.js;
+  document.getElementById("livecode").innerHTML = replaceAll(replaceAll(replaceAll(data.js, "\n", "<br/>"), "<", "&lt;"), ">", "&gt;");
 });
+function replaceAll(src,newr,oldr){
+  while(src.indexOf(oldr) > -1){
+    src = src.replace(oldr, newr);
+  }
+  return src;
+}
